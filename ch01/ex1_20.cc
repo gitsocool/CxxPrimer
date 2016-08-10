2e594ae3a116a908ce1a202668d4a1a253e4eff5
gitsocool <cdashen@126.com>
2016-08-10 14:40:22 +0800

    first to commit ch01

---
+++ ch01/ex1_20.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"/home/centos1/Documents/c++primer/GCC_pre_C11/1/Sales_item.h"
+int main(){
+ Sales_item si;
+
+while( std::cin >> si) std::cout << si << std::endl;
+
+return 0;
+ 
+}

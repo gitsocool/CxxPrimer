ac6a73e60b9aa679fd923a0b0e9b45fa2f1a0020
gitsocool <cdashen@126.com>
2016-08-18 01:28:21 +0800

    DerRefPoi

---
+++ ch02/DefAndPoi.cc
@@ -0,0 +1,38 @@
+iii#include<iostream>
+extern int i;
+int j;
+int main(){
+/* //Test for extern
+*   extern int i=8;
+*   std::cout << i<< std::endl;
+*   std::cout << j<< std::endl;
+*/
+
+   
+//Test for const qualifier
+   /*
+ *
+ *i
+ * 
+int a=7; 
+   const double b=a;
+   
+   double c=b;
+   
+   std::cout << b << "\t" << c << std::endl;  
+*/
+    
+//Test for reference 
+const double  hhh=878;
+const  int  &r=0,;
+const double &f;
+   f=hhh;
+
+ std::cout<< r << std::endl;  
+
+ 
+//Test for pointer
+
+
+  return 0;
+}
